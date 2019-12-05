f38f85b73c11793788f527ce7573f7861323b344
RameshNe <58315338+RameshNe@users.noreply.github.com>
2019-12-05 22:20:41 +0530

    Update App.js

--- behaviour/app/src/App.js
+++ behaviour/app/src/App.js
@@ -1,93 +1,106 @@
 import React, { Component } from "react";
 import logo from "./hinjewadi.jpg";
+import w_logo from "./hinjewadi-wipro.jpg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import locations from "./json/locations.json";
-import hotels from "./json/hotels.json";
-import Location from "./Location.js";
 import Hotel from "./Hotel.js";
-import FinalObject from "./FinalObject.js";
+import list from "./json/finalList.json";
 
 class App extends Component {
   state = {
     isLoading: false,
-    focus: false,
-    locations: locations,
-    hotels: hotels
+    locations: [],
+    hotels: [],
+    locationValue: "Location",
+    hotelValue: "Hotel",
+    finalList: [],
+    finalDisable: true,
+    isloadingDone: false
   };
-  _onBlur() {
-    setTimeout(() => {
-      if (this.state.focus) {
-        this.setState({
-          focus: false
-        });
-      }
-    }, 0);
-  }
-  _onFocus() {
-    if (!this.state.focus) {
-      this.setState({
-        focus: true
-      });
-    }
-  }
 
   constructor(props) {
     super(props);
-    //this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleSubmit(event) {}
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      finalList: list,
+      isLoading: false,
+      finalDisable: false,
+      isloadingDone: true
+    });
+  }
 
-  async componentDidMount() {
+  handleChange(event) {
+    this.setState({ locationValue: event.target.value });
+  }
+
+  async loadLocations() {
     const response = await fetch("/api/locations");
     const body = await response.json();
-    const hresponse = await fetch("/api/hotels");
-    const hbody = await hresponse.json();
-    this.setState({ locations: body, hotels: hbody, isLoading: false });
+    this.setState({ locations: body, isLoading: true });
   }
 
   render() {
-    const { locations, hotels, isLoading } = this.state;
-
-    if (isLoading) {
-      return <p>Loading...</p>;
-    }
-
     return (
       <form onSubmit={this.handleSubmit}>
         <div className="App">
           <header className="App-header" style={{ marginBottom: 20 }}>
             <h2>Map</h2>
-            <img src={logo} className="App-logo" alt="logo" />
+            {this.state.isLoading && !this.state.isloadingDone && (
+              <span>Loading...</span>
+            )}
+            {!this.state.isLoading && !this.state.isloadingDone && (
+              <img src={logo} className="App-logo" alt="logo" />
+            )}
+            {!this.state.isLoading && this.state.isloadingDone && (
+              <img src={w_logo} className="App-logo" alt="logo" />
+            )}
           </header>
           <div className="container" style={{ marginBottom: 20 }}>
             <div className="row">
               <div className="col-md-4"></div>
               <div className="col-md-4">
                 <h3>Location List</h3>
-                <Location
-                  locations={locations}
-                  hotels={hotels}
-                  //isLoading={this.setState({ isLoading: true })}
-                  //onFocus={this._onFocus()}
-                  //  onBlur={this._onBlur()}
-                />
+                <div>
+                  <select
+                    onChange={this.handleChange.bind(this)}
+                    style={{ width: `220px` }}
+                    onFocus={() => this.loadLocations()}
+                  >
+                    {Object.entries(this.state.locations).map(location => {
+                      return (
+                        <option key={location} value={JSON.stringify(location)}>
+                          {JSON.stringify(location)}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
                 <h3>Hotel List</h3>
-                <Hotel
-                  hotels={hotels}
-                  //isLoading={this.setState({ isLoading: true })}
-                  //onFocus={this._onFocus()}
-                  //onBlur={this._onBlur()}
-                />
+                <Hotel isLoading={this.state.isLoading} />
               </div>
             </div>
           </div>
           <div className="container" style={{ marginBottom: 40 }}>
             <h3>Final List</h3>
             <div>
-              <FinalObject hotels={hotels} />
+              <select style={{ width: `300px` }}>
+                {Object.entries(this.state.finalList).map(final => {
+                  return (
+                    <option
+                      key={final}
+                      value={final}
+                      disabled={this.state.finalDisable ? "disabled" : ""}
+                    >
+                      {JSON.stringify(final)}
+                    </option>
+                  );
+                })}
+              </select>
             </div>
           </div>
           <div className="container">
