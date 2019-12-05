69be4daa3513317c27e426c46f2540368fa38d18
RameshNe <58315338+RameshNe@users.noreply.github.com>
2019-12-05 22:22:39 +0530

    Hotel.js updates

--- behaviour/app/src/Hotel.js
+++ behaviour/app/src/Hotel.js
@@ -3,34 +3,43 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 class Hotel extends Component {
-  state = {
-    hotelValue: "Hotel"
-  };
   constructor(props) {
     super(props);
-    this.setState({
+    this.state = {
       isLoading: this.props.isLoading,
-      hotels: this.props.hotels
-    });
+      hotels: [],
+      hotelValue: this.props.hotelValue
+    };
   }
   handleChange(event) {
-    console.log("Hotel Event reached", event.target.value);
     this.setState({ hotelValue: event.target.value });
   }
+
+  async loadHotels() {
+    const response = await fetch("/api/hotels");
+    const body = await response.json();
+    this.setState({ hotels: body, isLoading: false });
+  }
+
   render() {
     return (
-      <select
-        onChange={this.handleChange.bind(this)}
-        style={{ width: `220px` }}
-      >
-        {Object.entries(this.props.hotels).map(hotel => {
-          return (
-            <option key={hotel} value={JSON.stringify(hotel)}>
-              {JSON.stringify(hotel)}
-            </option>
-          );
-        })}
-      </select>
+      <div>
+        <div>
+          <select
+            onChange={this.handleChange.bind(this)}
+            style={{ width: `220px` }}
+            onFocus={() => this.loadHotels()}
+          >
+            {Object.entries(this.state.hotels).map(hotel => {
+              return (
+                <option key={hotel} value={JSON.stringify(hotel)}>
+                  {JSON.stringify(hotel)}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      </div>
     );
   }
 }
